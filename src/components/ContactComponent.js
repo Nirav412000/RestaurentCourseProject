af747918356ac12e93f1899f68d2c2a42b074c70
Nirav Dadhaniya <nvd8866@gmail.com>
2020-07-14 03:51:26 +0530

    react-redux-form-validation

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -3,6 +3,12 @@ import {Breadcrumb, BreadcrumbItem , Button, Label, Col, Row } from 'reactstrap'
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors} from 'react-redux-form';
 
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9]+\.[A-Z]{2,4}$/i.test(val);
+
 class Contact extends Component{
 
     constructor(props){
@@ -70,7 +76,19 @@ class Contact extends Component{
                                 <Col md={10}>
                                     <Control.text model=".firstname" id="firstname" name="firstname" 
                                         placeholder="Enter First Name"
-                                        className="form-control" />
+                                        className="form-control" 
+                                        validators={{
+                                            required, minLength: minLength(2), maxLength: maxLength(15)
+                                        }} />
+                                    <Errors 
+                                        className="text-danger"
+                                        model=".firstname"
+                                        show="touched"
+                                        messages={{
+                                            required: 'required field. ',
+                                            minLength: 'This must be 2 character or more. ',
+                                            maxLength: 'This must be 15 character or less. ' 
+                                        }} />
                                 </Col>
                             </Row>
                             <Row className="form-group">
@@ -78,7 +96,19 @@ class Contact extends Component{
                                 <Col md={10}>
                                     <Control.text model=".lastname" id="lastname" name="lastname" 
                                         placeholder="Enter Last Name"
-                                        className="form-control" />
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(2), maxLength: maxLength(15)
+                                        }} />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".lastname"
+                                        show="touched"
+                                        messages={{
+                                            required : 'required field. ',
+                                            minLength : 'This must be 2 characters or more. ',
+                                            maxLength : 'This must be 15 characters or less. ' 
+                                        }} />
                                 </Col>
                             </Row>
                             <Row className="form-group">
@@ -86,7 +116,21 @@ class Contact extends Component{
                                 <Col md={10}>
                                     <Control.text model=".telnum" id="telnum" name="telnum" 
                                         placeholder="Enter Conatct Number"
-                                        className="form-control" />
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(10), maxLength: maxLength(10),
+                                            isNumber
+                                        }} />
+                                    <Errors 
+                                        className="text-danger"
+                                        model=".telnum"
+                                        show="touched"
+                                        messages={{
+                                            required : 'required field. ',
+                                            minLength : 'This must be of 10 characters. ',
+                                            maxLength : 'This must be of 10 characters. ',
+                                            isNumber : 'Only Digits allowed, No other Characters Please. ' 
+                                        }} />
                                 </Col>
                             </Row>
                             <Row className="form-group">
@@ -94,7 +138,18 @@ class Contact extends Component{
                                 <Col md={10}>
                                     <Control.text model=".email" id="email" name="email" 
                                         placeholder="Enter Email"
-                                        className="form-control" />
+                                        className="form-control"
+                                        validators={{
+                                            required, validEmail
+                                        }} />
+                                    <Errors 
+                                        className="text-danger"
+                                        model=".email"
+                                        show="touched"
+                                        messages={{
+                                            required : 'required field. ',
+                                            validEmail : 'Invalid Email Format. ' 
+                                        }} />
                                 </Col>
                             </Row>
                             <Row className="form-group">
